53ea34aab1aea6af64139c7d1a9978a6fda8303b
linderSeline <3268931779@qq.com>
2026-08-01 22:47:52 +0800

    尝试修改Qqbot

--- entry/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
+++ entry/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
@@ -80,6 +80,7 @@ export declare class ZlibBookInfoArk {
 }
 
 export declare class QQbot {
+    changeUrl(newUrl: string): void
     getBotQQGroup(): void
     getCommonGroup(qqNum: string, returnFunc: (funcArg0: Array<QQGroupInfoArk>) => void, errorFunc: (funcArg0: string) => void): void
     getConfig(returnFunc: (funcArg0: BotConfig) => void, errorFunc: (funcArg0: string) => void): void
